dbc903f1277f46e770920b0ac4110e1b753c1bd0
ironbas3 <basem.rizk@outlook.com>
2019-08-09 17:05:57 +0200

    eval test-other on intel-i7H

---
+++ prepare_tests.py
@@ -0,0 +1,85 @@
+"""
+Script to prepare tests for programatically evaluating paddlepaddle DeepSpeech2
+
+adapted from official repo librispeech.py script
+
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import soundfile
+import json
+import codecs
+
+
+import sys
+sys.path.insert(1, './PaddleSpeech')
+from data_utils.utility import download, unpack
+
+TARGET_DIR = "tests"
+MANIFEST_PREFIX = "tests"
+    
+URL_ROOT = "http://www.openslr.org/resources/12"
+URL_TEST_OTHER = URL_ROOT + "/test-other.tar.gz"
+MD5_TEST_OTHER = "fb5a50374b501bb3bac4815ee91d3135"
+
+def create_manifest(data_dir, manifest_path):
+    """Create a manifest json file summarizing the data set, with each line
+    containing the meta data (i.e. audio filepath, transcription text, audio
+    duration) of each audio file within the data set.
+    """
+    print("Creating manifest %s ..." % manifest_path)
+    json_lines = []
+    for folder, _, filelist in sorted(os.walk(data_dir)):
+        text_filelist = [
+            filename for filename in filelist if filename.endswith('trans.txt')
+        ]
+        if len(text_filelist) > 0:
+            text_filepath = os.path.join(folder, text_filelist[0])
+            for line in open(text_filepath):
+                segments = line.strip().split()
+                text = ' '.join(segments[1:]).lower()
+                audio_filepath = os.path.join(folder,
+                                              segments[0] + '.wav')
+                audio_data, samplerate = soundfile.read(audio_filepath)
+                duration = float(len(audio_data)) / samplerate
+                json_lines.append(
+                    json.dumps({
+                        'audio_filepath': audio_filepath,
+                        'duration': duration,
+                        'text': text
+                    }))
+    with codecs.open(manifest_path, 'w', 'utf-8') as out_file:
+        for line in json_lines:
+            out_file.write(line + '\n')
+
+
+def prepare_dataset(url, md5sum, target_dir, manifest_path):
+    """Download, unpack and create summmary manifest file.
+    """
+    data_dir = os.path.join(target_dir, "LibriSpeech")
+    if not os.path.exists(data_dir):
+        # download
+        filepath = download(url, md5sum, target_dir)
+        # unpack
+        unpack(filepath, target_dir)
+    else:
+        print("Skip downloading and unpacking. Data already exists in %s." %
+              data_dir)
+    # create manifest json file
+    create_manifest(data_dir, manifest_path)
+
+
+def main():
+    prepare_dataset(
+        url=URL_TEST_OTHER,
+        md5sum=MD5_TEST_OTHER,
+        target_dir=TARGET_DIR,
+        manifest_path=MANIFEST_PREFIX + ".test-other")
+    
+
+
+if __name__ == '__main__':
+    main()
--- prog_paddle.py
+++ prog_paddle.py
@@ -1,30 +1,39 @@
 # -*- coding: utf-8 -*-
 
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import paddle.v2 as paddle
+
+import sys
+sys.path.insert(1, './PaddleSpeech')
 from data_utils.data import DataGenerator
 from model_utils.model import DeepSpeech2Model
 from utils.error_rate import char_errors, word_errors
 
 from os import path, makedirs
-from jiwer import wer    
 import soundfile as sf
-import sys
+
 
 from timeit import default_timer as timer
-from utils import get_platform_id, document_machine
-from utils import prepare_pathes, get_metafiles_pathes
+from basic_utils import get_platform_id, document_machine
+from basic_utils import prepare_pathes, get_metafiles_pathes
 
-MODEL_VERSION="librispeech"
 
 TEST_PATH="tests/LibriSpeech/test-other"
 #TEST_PATH="tests/iisys"
+MODEL_VERSION="librispeech"
 
+model_dir = "PaddleSpeech/models"
 
-test_manifest = path.join(TEST_PATH, 'manifest.test-clean')
-mean_std_path = path.join(TEST_PATH,'mean_std.npz')
-vocab_path = path.join('models',  MODEL_VERSION, 'vocab.txt')
-model_path= path.join('models',  MODEL_VERSION, 'params.tar.gz' )
-lang_model_path= path.join('models',  MODEL_VERSION, 'common_crawl_00.prune01111.trie.klm')
+test_manifest = 'manifest.' + TEST_PATH.split("/")[-1]
+
+mean_std_path = path.join(model_dir, MODEL_VERSION,'mean_std.npz')
+vocab_path = path.join(model_dir,  MODEL_VERSION, 'vocab.txt')
+model_path= path.join(model_dir,  MODEL_VERSION, 'params.tar.gz' )
+lang_model_path= path.join(model_dir,  "lm", 'common_crawl_00.prune01111.trie.klm')
 
 IS_GLOBAL_DIRECTORIES = True
 USING_GPU = False
@@ -108,6 +117,9 @@ text_pathes.sort()
 ##############################################################################
 # ----------------------------- Model Loading 
 ##############################################################################
+log_file = open(log_filepath, "w")
+summ_file = open(summ_filepath, "w")
+
 batch_size=1 
 trainer_count=1 
 beam_size=500 
@@ -143,6 +155,9 @@ batch_reader = data_generator.batch_reader_creator(
     sortagrad=False,
     shuffle_method=None)
 
+print('Loading inference model from files {}'.format(model_path))
+log_file.write('Loading inference model from files {}'.format(model_path))
+inf_model_load_start = timer()
 ds2_model = DeepSpeech2Model(
     vocab_size=data_generator.vocab_size,
     num_conv_layers=num_conv_layers,
@@ -151,27 +166,62 @@ ds2_model = DeepSpeech2Model(
     use_gru=USING_GRU,
     pretrained_model_path=model_path,
     share_rnn_weights=share_rnn_weights)
+inf_model_load_end = timer() - inf_model_load_start
+print('Loaded inference model in {:.3}s.'.format(inf_model_load_end))
+log_file.write('Loaded inference model in {:.3}s.'.format(inf_model_load_end))
+summ_file.write('Loaded inference model in,{:.3}s.'.format(inf_model_load_end))
 
 # decoders only accept string encoded in utf-8
 vocab_list = [chars.encode("utf-8") for chars in data_generator.vocab_list]
 
 if decoding_method == "ctc_beam_search":
+    print('Loading language model (scorer) from files {}'.format(lang_model_path),
+          file=sys.stderr)
+    log_file.write('Loading language model (scorer) from files {}'.format(lang_model_path))
+    lm_load_start = timer()
     ds2_model.init_ext_scorer(alpha, beta, lang_model_path,
                               vocab_list)
+    lm_load_end = timer() - lm_load_start
+    print('Loaded language model (scorer)) in {:.3}s.'.format(lm_load_end))
+    log_file.write('Loaded language model (scorer) in {:.3}s.'.format(lm_load_end))
+    summ_file.write('Loaded language model (scorer) in,{:.3}s.'.format(lm_load_end))
+    
+    
 errors_func = char_errors if error_rate_type == 'cer' else word_errors
 errors_sum, len_refs, num_ins = 0.0, 0, 0
 
 ##############################################################################
 # ---Running the PADDLE DS2 STT Engine by running through the audio files
 ##############################################################################
+import pandas as pd
+audio_files = pd.read_json(test_manifest, lines=True)
 processed_data = "filename,length(sec),proc_time(sec),wer,actual_text,processed_text\n"
 avg_wer = 0
 avg_proc_time = 0
 num_of_audiofiles = len([item for sublist in audio_pathes for item in sublist])
 current_audio_number = 1
-all_text_pathes = [item for sublist in text_pathes for item in sublist]
 
-for infer_data in batch_reader():
+for infer_data, sample_audio_path, imported_text in \
+            zip(batch_reader(), audio_files["audio_filepath"], audio_files["text"]):
+    
+    print("\n=> Progress = " + "{0:.2f}".format((current_audio_number/num_of_audiofiles)*100) + "%\n" )
+    current_audio_number+=1
+    
+    infer_data_text = infer_data[0][1]
+    if infer_data_text != imported_text:
+        print("WARNING \n")
+        print("infer_data_text: \n")
+        print(infer_data_text + "\n")
+        print("imported_text: \n")
+        print(imported_text + "\n")
+        break
+        
+    audio, fs = sf.read(sample_audio_path, dtype='int16')
+    audio_len = len(audio)/fs 
+        
+    print('Running inference.', file=sys.stderr)
+    inference_start = timer()
+
     probs_split = ds2_model.infer_batch_probs(
         infer_data=infer_data,
         feeding_dict=data_generator.feeding)
@@ -190,7 +240,11 @@ for infer_data in batch_reader():
             cutoff_top_n=cutoff_top_n,
             vocab_list=vocab_list,
             num_processes=num_proc_bsearch)
-        
+
+    inference_end = timer() - inference_start
+    print('Inference took %0.3fs for %0.3fs audio file.' % (inference_end, audio_len))       
+    proc_time = round(inference_end, 3)
+    
     target_transcripts = [data[1] for data in infer_data]
     
     for target, result in zip(target_transcripts, result_transcripts):
@@ -200,6 +254,40 @@ for infer_data in batch_reader():
         num_ins += 1
     print("Error rate [%s] (%d/?) = %f" %
           (error_rate_type, num_ins, errors_sum / len_refs))
+
+    # Processing WORD ERROR RATE (WER)
+    actual_text = imported_text.lower()
+    current_wer = -1 #wer(actual_text, processed_text, standardize=True)
+    current_wer = round(current_wer,3)
+    
+    # Accumlated data
+    avg_proc_time += (proc_time/(audio_len))
+    avg_wer += current_wer
+    
+    processed_text = result_transcripts[0]
+    progress_row = sample_audio_path + "," + str(audio_len) + "," + str(proc_time)  + "," +\
+                    str(current_wer) + "," + actual_text + "," + processed_text
+                     
+    if(VERBOSE):
+        print("# Audio number " + str(current_audio_number) + "/" + str(num_of_audiofiles) +"\n" +\
+              "# File (" + sample_audio_path + "):\n" +\
+              "# - " + str(audio_len) + " seconds long.\n"+\
+              "# - actual    text: '" + actual_text + "'\n" +\
+              "# - processed text: '" + processed_text + "'\n" +\
+              "# - processed in "  + str(proc_time) + " seconds.\n"
+              "# - WER = "  + str(current_wer) + "\n")
+              
+    log_file.write("# Audio number " + str(current_audio_number) + "/" + str(num_of_audiofiles) +"\n" +\
+          "# File (" + sample_audio_path + "):\n" +\
+          "# - " + str(audio_len) + " seconds long.\n"+\
+          "# - actual    text: '" + actual_text + "'\n" +\
+          "# - processed text: '" + processed_text + "'\n" +\
+          "# - processed in "  + str(proc_time) + " seconds.\n"
+          "# - WER = "  + str(current_wer) + "\n")
+    
+              
+    processed_data+= progress_row + "\n"
+    
 print("Final error rate [%s] (%d/%d) = %f" %
       (error_rate_type, num_ins, num_ins, errors_sum / len_refs))
 
